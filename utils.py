b182b3d97c74764027e64792509ca23b35915a13
Yous <joseflores@alumnos.uai.cl>
2024-11-28 14:23:23 -0300

    Made changes for myself

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import random
 
+SEED = 0
 
 def plot_number(idx, data):
 
@@ -18,8 +22,17 @@ def plot_training_curves(train_loss, validation_loss, n_epochs, title = ""):
 
 ## Es importante cambiar el orden de los canales para poder mostrar la imagen...
 def plot_images(idx, data):
+    
     plt.imshow(data[idx][0].permute(1, 2, 0))
     class_label = data[idx][1]
     plt.title(data.classes[class_label])
     plt.axis("off")
     print(data[idx][0].shape)
+
+def set_seed(seed = SEED):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
